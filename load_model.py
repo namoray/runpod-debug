3ac3d07db607edd437bab32762bd2dcc613472db
namoray <neuvaamor@proton.me>
2024-04-10 17:02:10 +0000

    Explained more

--- load_model.py
+++ load_model.py
@@ -11,4 +11,5 @@ safety_pipe = StableDiffusionPipeline.from_pretrained(
 path, torch_dtype=torch.bfloat16
 ).to(device)
 
-print("Time to load (should be very small amount of seconds):", time.time() - time1)
+print("Time to load (should be very small amount of seconds after the initial download):", time.time() - time1)
+print("Look in your console logs. If you see more than ~2s per step to load the model, there is an issue")
