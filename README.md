377562cccf196fa6872f22a683313573e20bc8c7
namoray <neuvaamor@proton.me>
2024-04-10 17:06:25 +0000

    all docs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # runpod-debug
+
+
+Try to run the load_model.py script. After the initial download of stuff (which is fine), take note of the loading time of the model.
+
+On good hardware, this takes ~5s to load the whole model. On bad hardware (is my theory), it takes 60 seconds. Why?
+
+```bash
+python -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
+python load_model.py
+```
--- load_model.py
+++ load_model.py
@@ -11,5 +11,6 @@ safety_pipe = StableDiffusionPipeline.from_pretrained(
 path, torch_dtype=torch.bfloat16
 ).to(device)
 
-print("Time to load (should be very small amount of seconds after the initial download):", time.time() - time1)
+print("Time to load: ", time.time() - time1)
+print("If this was your first time running, run it again please as the download tainted it, or:")
 print("Look in your console logs. If you see more than ~2s per step to load the model, there is an issue")
